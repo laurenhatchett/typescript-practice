47c01c2e7a989baa9ea9c52d7e0959216c01aac1
laurenhatchett <laurenhatchett@gmail.com>
2023-04-22 13:36:26 -0400

    compile JS file

---
+++ lesson 2/build/js/main.js
@@ -0,0 +1,14 @@
+var myName = "Lauren";
+var meaningofLife;
+var isLoading;
+var album;
+myName = "Lauren";
+meaningofLife = 42;
+isLoading = true;
+album = 5150;
+var sum = function (a, b) {
+    return a + b;
+};
+var postId;
+var isActive;
+var re = /\w+/g;
