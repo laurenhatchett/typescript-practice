e00d17d72aa489803c3347e05adbe0cfc8c1e3c3
laurenhatchett <laurenhatchett@gmail.com>
2023-04-22 13:31:16 -0400

    practice types

---
+++ lesson 2/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Lauren"
+let meaningofLife: number
+let isLoading: boolean
+let album: any
+
+myName = "Lauren"
+meaningofLife = 42
+isLoading = true
+album = 5150
+
+const sum = (a:number, b:string) => {
+  return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
